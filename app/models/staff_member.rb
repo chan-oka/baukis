dfb5c584eb7f12e4b44be168374f07b4d7f4ef71
chanoka <chanoka2016@gmail.com>
2016-08-11 21:58:12 +0000

    modelの学習

--- app/models/staff_member.rb
+++ app/models/staff_member.rb
@@ -1,2 +1,15 @@
 class StaffMember < ActiveRecord::Base
+	before_validation do
+		self.email_for_index = email.downcase if email
+	end
+
+	def password=(raw_password)
+		# Stringオブジェクトかチェック
+		if raw_password.kind_of?(String)
+			# パスワードをハッシュ化
+			self.hashed_password = BCrypt::Password.create(raw_password)
+		elsif raw_password.nil?
+			self.hashed_password = nil
+		end
+	end
 end
